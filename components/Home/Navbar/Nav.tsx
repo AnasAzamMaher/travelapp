eba8d111d1c0350592d5e92f4517f7160970587a
Anas Azam <anasazammaher@gmail.com>
2024-12-17 15:26:34 +0530

    final commits

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import { HiBars3BottomRight } from 'react-icons/hi2';
 import { TbAirBalloon } from 'react-icons/tb';
-import { Link as ScrollLink } from 'react-scroll';
 
 type props = {
   openNav: ()=> void
@@ -12,6 +11,12 @@ type props = {
 
 const Nav = ({openNav}:props) => {
   const [navBg, setNavBg] = useState(false)
+
+  const handleClick=(link: string)=>{
+    const id = link.split("#").join('')
+    const elem = document.getElementById(id)
+    elem?.scrollIntoView( {block: "center", inline: "nearest",behavior: "smooth"})
+  }
 
 
   useEffect(() => {
@@ -40,9 +45,9 @@ const Nav = ({openNav}:props) => {
         <div className='hidden lg:flex items-center space-x-10'>
           {
             NavLinks.map((link) => {
-              return <ScrollLink smooth={true} to={link.url.replace("#", "")} duration={400} offset={-80}  key={link.id} >
+              return <Link href={link.url} key={link.id} scroll={false} onClick={() => handleClick(link.url)}>
                 <p className="relative text-white text-base font-medium w-fit cursor-pointer block after:block after:content-[''] after:absolute after:h-[3px] after:bg-yellow-300 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition-all duration-300 after:origin-left">{link.label}</p>
-              </ScrollLink>
+              </Link>
             })
           }
         </div>
